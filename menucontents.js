0198e3114d77d6ce7953d98789d07dd13bbb928e
wwwdaintee <68058243+wwwdaintee@users.noreply.github.com>
2020-07-09 16:59:56 +0530

    Add files via upload

---
+++ menucontents.js
@@ -0,0 +1,41 @@
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	["Sweets & Toffee Products", "products_sweetstoffees.html"],
+	["Cocoa Based Confectionery", "products_chocolates.html"],
+	["Jelly Products", "products_jellybased.html"],
+	["Gum Products", "products_bubblegum.html"],
+	["Biscuit Products", "products_biscuits.html"],
+	["Tea Products", "products_tea.html"],
+	["Snacks & Cereal Products", "products_snackserials.html"],
+	["Gift Products", "products_gift assortments.html"],
+	["Virgin Coconut Oil & Others", "products_Virgin Coconu oilothers.html"] //no comma following last entry!
+]
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu2.items=[
+	["Daintee Rasa Pana", "daintee rasapana.html"],
+	["X-tra Lama Tharu", "daintee x-tra lamatharu.html"],
+	["X-tra 40+", "daintee x-tra 40plus.html"],
+	["Commercial Ads", "daintee commercial ads.html"],
+	["Newspaper Articles", "daintee newspaper articles.html"] //no comma following last entry!
+]
+
+
+
+var anylinkmenu3={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Third menu variable. Same precaution.
+anylinkmenu3.cols={divclass:'column', inlinestyle:''} //menu.cols if defined creates columns of menu links segmented by keyword "efc"
+anylinkmenu3.items=[
+	["Daintee Limited History", "daintee group (daintee_limited).html"],
+	["Marketing Department", "daintee group (daintee_marketing_limited).html"],
+	["Quality Department", "daintee group (quality_department).html"],
+	["Trophy Cabinet", "daintee group (trophy_cabinet).html"],
+	["Retail Partners", "daintee group (retail_partners).html"] //no comma following last entry!
+]
+
+var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'width:150px; background:#DFFDF4', linktarget:'_new'} //Second menu variable. Same precaution.
+anylinkmenu4.items=[
+	["CNN", "http://www.cnn.com/"],
+	["MSNBC", "http://www.msnbc.com/"],
+	["Google", "http://www.google.com/"],
+	["BBC News", "http://news.bbc.co.uk"] //no comma following last entry!
+]
